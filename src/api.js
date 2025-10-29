82fc756c47e4331f1739862ed5a57813531cc37c
korniplodiks <korniplodiks@users.noreply.github.com>
2025-10-29 15:59:32 +0200

    ci: fix null pointer exception

--- src/api.js
+++ src/api.js
@@ -266,3 +266,30 @@ class API {
 
 // Update 55
 module.exports = API;
+
+
+// API module for AdventureGame
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 56
+module.exports = API;
